b114f70bd0d464eff09bf48c5c20f3cbfa80881e
Tushar kumar <narutoboi41@gmail.com>
2026-05-09 15:54:55 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <div>
         <Navbar/>
         <Switch>
-            <Route path='/' component={Home}/>
+            <Route exact path='/' component={Home}/>
             <Route path='/about' component={About}/>
         </Switch>
     </div>
